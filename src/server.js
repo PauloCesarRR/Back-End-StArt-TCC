7533916a57f9d53e138b7c4577487c373679ee91
BlVck213 <paulocesarrrpontes@gmail.com>
2022-05-30 11:18:22 -0300

    Chat em desenvolvimento

--- src/server.js
+++ src/server.js
@@ -21,13 +21,56 @@ app.use('/', (req, res) => {
 let messages = []
 
 io.on('connection', socket => {
+
+    messages = []
+
+    idChat = 1
+
+    mysql.getConnection((error, conn) => {
+        if (error) { return console.log(error) }
+        conn.query('SELECT * FROM tblMensagem WHERE idChat = ?', [idChat], 
+        (error, results) => {
+
+            if (error) { return console.log(error) }
+
+            if (results.length == 0){
+                socket.emit('messages', messages)
+            }
+
+            const response = {
+                qtdMensagens: results.length,
+                mensagem: results.map(mensagem => {
+                    return {
+                        idChat: mensagem.idChat,
+                        mensagem: mensagem.mensagem,
+                        foto: mensagem.foto,
+                        data_hora: mensagem.data_hora,
+                        artistaOUcliente: mensagem.artistaOUcliente,
+                        idUsuario: mensagem.idUsuario
+                    }
+                })
+            }
+            mysql.releaseConnection(conn)
+           return messages = response        
+        })
+    })
+
+
     console.log(`Socket conectado: ${socket.id}`)
 
     socket.emit('previousMessages', messages)
 
     socket.on('sendMessage', data => {
 
-        messages.push(data)
+        mysql.getConnection((error, conn) => {
+            if (error) { return console.log(error) }
+            conn.query('INSERT INTO tblMensagem (idChat, mensagem, foto, data_hora, artistaOUcliente, idUsuario) VALUES (?, ?, ?, ?, ?, ?)', [idChat, data.mensagem, data.foto, data.data_hora, data.artistaOUcliente, data.idUsuario],
+            (error, results) => {
+                if (error) { return console.log(error) }
+                mysql.releaseConnection(conn)
+            })
+        })
+        
         socket.broadcast.emit('receivedMessage', data)
     })
 })
